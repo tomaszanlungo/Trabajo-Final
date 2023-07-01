473024c7ff3b6e5c564e3d5ebcdd769e647cd8cb
tomaszanlungo <tomaszanlungo@gmail.com>
2023-07-01 11:07:43 -0300

    Actualizacion de repo (archivos nuevos)

---
+++ README.md
@@ -0,0 +1,12 @@
+# VD | Maqueta Scrolly de Fotos - Scrollama + D3
+
+![scroll fotos](scroll_foto.gif)
+
+
+## Librería para crear scrollytellings
+
+[Scrollama.js](https://github.com/russellsamora/scrollama)
+
+## D3 Selection
+[D3 Selection](https://github.com/d3/d3-selection)
+
---
+++ scripts/index.js
@@ -0,0 +1,175 @@
+window.addEventListener('scroll', function () {
+
+  var header = document.querySelector('header')
+
+  header.classList.toggle('abajo', window.scrollY > 0)
+
+})
+
+
+// Archivo: script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  function processData(data) {
+
+    // Crea un objeto para almacenar los datos por hora
+
+    const dataByHour = []
+
+
+    // Recorre los datos y agrupa las horas y la cantidad de horas escuchadas
+
+    data.forEach(entry => {
+
+      const endTime = new Date(entry.endTime)
+
+      const hour = endTime.getHours()
+
+      const msPlayed = entry.msPlayed
+
+      const hoursPlayed = msPlayed / (1000 * 60 * 60) // Convierte milisegundos a horas
+
+
+      dataByHour.push({ hour, hoursPlayed })
+
+    })
+
+
+    return dataByHour
+
+  }
+
+
+  // Obtén los datos del archivo JSON
+
+  fetch('datos/StreamingHistory0.json')
+
+    .then(response => response.json())
+
+    .then(data => {
+
+      // Procesa los datos y crea los grupos por hora
+
+      const dataByHour = processData(data)
+
+
+      // Crea el gráfico de barras
+
+      const plot = Plot.plot({
+
+        marks: [
+
+          Plot.barY(dataByHour, 
+
+            /* Carlos: Faltaba sumar las horas */
+
+            Plot.groupX({y: "sum"}, {x: "hour", y: "hoursPlayed", fill: "#FF84BD"})
+
+            )
+
+          ],
+
+        height: 500,
+        width: 900,
+
+        x: {
+
+          label: '',
+          ticks: 24,
+        },
+
+        y: {
+
+          label: 'Horas escuchadas',
+
+          ticks: 10,
+
+        },
+
+      })
+
+      
+
+      /* 
+
+      Carlos: El append del plot debe estar dentro del then(),
+
+      cuando sabemos que los datos está resuelto 
+
+      */
+
+      const chartContainer = document.getElementById("chart");
+      
+
+      chartContainer.appendChild(plot);
+
+    })
+
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const columns = document.querySelectorAll('.column');
+
+  function checkVisibility() {
+    columns.forEach(column => {
+      const rect = column.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      // Comprueba si el elemento está en el viewport
+      if (rect.top < windowHeight) {
+        column.classList.add('animate');
+      }
+    });
+  }
+
+  // Ejecuta la función inicialmente para comprobar la visibilidad al cargar la página
+  checkVisibility();
+
+  // Agrega un event listener al evento scroll para comprobar la visibilidad mientras se desplaza la página
+  window.addEventListener('scroll', checkVisibility);
+});
+
+
+
+/* https://github.com/russellsamora/scrollama */
+// Creamos variables globales
+// Usamos d3 para seleccionar elementos del DOM
+let main = d3.select("main");
+let scrolly = main.select("#scrolly_fotos");
+let $figure = scrolly.select("#fixed_container");
+
+// Creamos una instancia de scrollama
+let scroller = scrollama();
+
+// Inicializamos el proceso de scrollama
+init();
+
+function init() {
+  // 1. Configuramos las opciones de scrollama
+  // 2. Vinculamos las funciones de scrollama con los eventos cuando entra y/o sale un step
+  scroller
+    .setup({
+      step: "#scrolly_fotos #steps_container .step",
+      offset: 0.9, // 0 a 1 para el porcentaje de la pantalla
+      debug: false,
+      progress: false,
+    })
+    .onStepEnter(handleFotos)
+}
+
+// scrollama event handlers
+function handleFotos(response) {
+  // response retorna el elemento (step), la dirección (up o down) y el índice del step
+  // response = { element, direction, index }
+  console.log(response)
+
+  /* Ocultamos todas las fotos del scrolly */
+  d3.selectAll(".foto").classed("is_visible", false)
+
+  /* Mostramos la foto correspondiente al step */
+  d3.select(`#foto_${response.index}`).classed("is_visible", true)
+}
+
+
+
